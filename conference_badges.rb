49740614b8067932ae3c5b192ad56dd177e4976e
hailuhr <hredleaf@gmail.com>
2016-05-09 15:11:06 +0000

    nitrous-backup at Mon May  9 15:11:06 UTC 2016

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+# Write your code here
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new = []
+  array.each do |name|
+    message = "Hello, my name is #{name}."
+    new << message
+  end
+end
